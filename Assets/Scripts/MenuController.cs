1c169ceac2c2c9fe0175f64b91c624ee6be78eed
Jon Lee <agentlee927@gmail.com>
2019-01-27 00:24:46 -0500

    title'

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    public Button startButton, controlsButton, backButton;
+    public Transform controls;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton.onClick.AddListener(delegate { StartGame(); });
+        controlsButton.onClick.AddListener(delegate { ShowControls(); });
+        backButton.onClick.AddListener(delegate { ShowControls(); });
+    }
+
+    void StartGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    void ShowControls()
+    {
+        controls.gameObject.SetActive(!controls.gameObject.activeSelf);
+    }
+}
